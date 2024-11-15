c6de683a8d25b1460125b25903d9afc8040e5eec
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2024-11-14 20:04:25 -0500

    mejorando el componente y cambiandolo

--- dist/components/bubbles/SourceBubble.d.ts
+++ dist/components/bubbles/SourceBubble.d.ts
@@ -1,7 +1,7 @@
 type Props = {
     pageContent: string;
     metadata: object;
-    onSourceClick?: () => void;
+    onSourceClick: () => void;
 };
 export declare const SourceBubble: (props: Props) => import("solid-js").JSX.Element;
 export {};
--- src/components/bubbles/SourceBubble.tsx
+++ src/components/bubbles/SourceBubble.tsx
@@ -1,32 +1,26 @@
 type Props = {
   pageContent: string;
   metadata: object;
-  onSourceClick?: () => void;
+  onSourceClick: () => void;
 };
 export const SourceBubble = (props: Props) => (
-  <>
-    <div
-      data-modal-target="defaultModal"
-      data-modal-toggle="defaultModal"
-      class="flex justify-start mb-2 items-start animate-fade-in host-container hover:brightness-90 active:brightness-75"
-      onClick={() => props.onSourceClick?.()}
+  <div class="flex justify-start mb-2 items-start animate-fade-in host-container">
+    <button
+      type="button"
+      onClick={()=>props.onSourceClick()}
+      class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
+      style={{
+        width: 'max-content',
+        'max-width': '80px',
+        'font-size': '13px',
+        'border-radius': '15px',
+        cursor: 'pointer',
+        'text-overflow': 'ellipsis',
+        overflow: 'hidden',
+        'white-space': 'nowrap',
+      }}
     >
-      <span
-        class="px-2 py-1 ml-1 whitespace-pre-wrap max-w-full chatbot-host-bubble"
-        data-testid="host-bubble"
-        style={{
-          width: 'max-content',
-          'max-width': '80px',
-          'font-size': '13px',
-          'border-radius': '15px',
-          cursor: 'pointer',
-          'text-overflow': 'ellipsis',
-          overflow: 'hidden',
-          'white-space': 'nowrap',
-        }}
-      >
-        {props.pageContent}
-      </span>
-    </div>
-  </>
+    lala  {props.pageContent}
+    </button>
+  </div>
 );
